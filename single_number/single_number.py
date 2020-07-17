2fe7b1f537202bb037971ed281c7e70f2bd07145
Zernach <ryan@zernach.com>
2020-07-16 23:20:13 -0400

    adds single_number solution

--- single_number/single_number.py
+++ single_number/single_number.py
@@ -3,9 +3,22 @@ Input: a List of integers where every int except one shows up twice
 Returns: an integer
 '''
 def single_number(arr):
-    # Your code here
 
-    pass
+    # Instantiate new list
+    count_list = []
+
+    # For each number in the array... check if it's in the count_list already
+    # If it is, then delete it from the count_list
+    # If it is not, then add it to the count_list
+    # After going through entire original array/list,
+    # the count_list should only have one value in it, so return it!
+    for num in arr:
+        if num in count_list:
+            count_list.remove(num)
+        else:
+            count_list.append(num)
+
+    return count_list[0]
 
 
 if __name__ == '__main__':
