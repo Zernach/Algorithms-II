3fc7d5ba23c946c08318da97c97076520952b318
Zernach <ryan@zernach.com>
2020-07-16 23:45:45 -0400

    adds moving_zeroes solution

--- moving_zeroes/moving_zeroes.py
+++ moving_zeroes/moving_zeroes.py
@@ -3,9 +3,23 @@ Input: a List of integers
 Returns: a List of integers
 '''
 def moving_zeroes(arr):
-    # Your code here
+    
+    # Create a copy of the array
+    # And start counting how many zeros are in the array
+    new_arr = list(arr)
+    num_of_zeros = 0
 
-    pass
+    # For each number in array, if it's a zero, remove it, and +1 to count
+    for num in arr:
+        if num == 0:
+            num_of_zeros += 1
+            new_arr.remove(num)
+
+    # Add zeros to end of list, according to how many were found in original array
+    for i in range(0, num_of_zeros):
+        new_arr.append(0)
+        
+    return new_arr
 
 
 if __name__ == '__main__':
